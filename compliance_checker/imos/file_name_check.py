00b3302cba9bc5c9705b8024f517dd7425971e29
ming fu <fxmzb123@gmail.com>
2015-05-13 15:17:46 +1000

    Added support to check_file_name_field1 and check_file_name_field3. Added missing docstring for check_extension_name and check_file_name.

--- compliance_checker/imos/file_name_check.py
+++ compliance_checker/imos/file_name_check.py
@@ -5,6 +5,7 @@ http://www.imos.org.au/
 '''
 
 import os.path
+import re
 
 from compliance_checker.base import BaseCheck, BaseNCCheck, Result
 
@@ -31,6 +32,9 @@ class IMOSFileNameCheck(BaseNCCheck):
             self._file_extension_name = file_names[-1]
 
     def check_extension_name(self, ds):
+        '''
+        Check file extension name and ensure it equals to nc
+        '''
         ret_val = []
         result_name = ['file_name','check_extension_name']
         reasoning = ["File extension name is not equal to nc"]
@@ -45,6 +49,9 @@ class IMOSFileNameCheck(BaseNCCheck):
         return ret_val
 
     def check_file_name(self, ds):
+        '''
+        Check file name and ensure it contains 6 to 10 fields, separated by '_'
+        '''
         ret_val = []
         result_name = ['file_name','check_file_name']
         reasoning = ["File name doesn't contain 6 to 10 fields, separated by '_'"]
@@ -53,6 +60,50 @@ class IMOSFileNameCheck(BaseNCCheck):
 
         if len(file_names) >= 6 and len(file_names) <= 10:
             result = Result(BaseCheck.HIGH, True, result_name, None)
+        else:
+            result = Result(BaseCheck.HIGH, False, result_name, reasoning)
+
+        ret_val.append(result)
+
+        return ret_val
+
+    def check_file_name_field1(self, ds):
+        '''
+        Check file name field1 and ensure it is "IMOS"
+        '''
+        ret_val = []
+        result_name = ['file_name','check_file_name_field1']
+        reasoning = ["File name field1 is not 'IMOS'"]
+
+        file_names = [ name for name in self._file_name.split('_') ]
+
+        if len(file_names) >= 0:
+            if file_names[0] != 'IMOS':
+                result = Result(BaseCheck.HIGH, False, result_name, reasoning)
+            else:
+                result = Result(BaseCheck.HIGH, True, result_name, None)
+        else:
+            result = Result(BaseCheck.HIGH, False, result_name, reasoning)
+
+        ret_val.append(result)
+
+        return ret_val
+
+    def check_file_name_field3(self, ds):
+        '''
+        Check file name field3 and ensure it is made up of the characters "ABCEFGIKMOPRSTUVWZ"
+        '''
+        ret_val = []
+        result_name = ['file_name','check_file_name_field3']
+        reasoning = ["File name field3 is not made up of characters 'ABCEFGIKMOPRSTUVWZ'"]
+
+        file_names = [ name for name in self._file_name.split('_') ]
+
+        if len(file_names) >= 2:
+            if re.search('^[ABCEFGIKMOPRSTUVWZ]+$', file_names[2]) == None:
+                result = Result(BaseCheck.HIGH, False, result_name, reasoning)
+            else:
+                result = Result(BaseCheck.HIGH, True, result_name, None)
         else:
             result = Result(BaseCheck.HIGH, False, result_name, reasoning)
 
