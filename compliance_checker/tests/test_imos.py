b8d576c6b3a8cc7a322967dc5b23a1d3b740f71e
ming fu <fxmzb123@gmail.com>
2015-06-25 16:16:34 +1000

    Added check_quality_control_conventions_for_quality_control_variable.

--- compliance_checker/tests/test_imos.py
+++ compliance_checker/tests/test_imos.py
@@ -451,6 +451,15 @@ class TestIMOS(unittest.TestCase):
 
         self.assertTrue(ret_val[0].value)
         self.assertTrue(ret_val[1].value)
+        
+    def test_check_quality_control_conventions_for_quality_control_variable(self):
+        self.imos.setup(self.test_variable_dataset)
+        ret_val = self.imos.check_quality_control_conventions_for_quality_control_variable(self.test_variable_dataset)
+
+        self.assertTrue(ret_val != None)
+        self.assertTrue(len(ret_val) == 1)
+
+        self.assertTrue(ret_val[0].value)
 
     def test_check_quality_control_set_for_quality_control_variable(self):
         self.imos.setup(self.test_variable_dataset)
